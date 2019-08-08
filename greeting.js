859167d9fc91d12965a345c59dabe2dbf7356aca
Tim Weiss <tdweiss1978@gmail.com>
2019-08-08 10:35:26 -0700

    Initial Commit

---
+++ greeting.js
@@ -0,0 +1,8 @@
+exports.greetingEnglish = function() {
+  console.log("Hello");
+}
+
+exports.greetingJapanese = function() {
+  console.log("Konichiwa");
+}
+
---
+++ main.js
@@ -0,0 +1,7 @@
+const greetings = require("./greeting.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
